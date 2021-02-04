b1cebeb842c34e4dd6c74e202066b34002d330e0
Olivier Le Page <olivier.le-page@capgemini.com>
2021-02-04 12:45:08 +0100

    Move PushBullet token to config vars

--- TODO.md
+++ TODO.md
@@ -5,3 +5,4 @@ Todo list:
 - Add Pushbullet notification capability
 - Add mail alert upon API call failure
 - Centralize logging config (import logger https://docs.python.org/3/howto/logging.html)
+- Check return code from push notification & alert if failure
--- common/constants.py
+++ common/constants.py
@@ -1,3 +1,5 @@
+import os
+
 debug = True
 rain_token = '__Wj7dVSTjV9YGu1guveLyDq0g7S7TfTjaHBTPTpO0kj8__'
 rain_url = 'https://rpcache-aa.meteofrance.com/internet2018client/2.0/nowcast/' \
@@ -5,4 +7,6 @@ rain_url = 'https://rpcache-aa.meteofrance.com/internet2018client/2.0/nowcast/' 
 default_lat = 48.571926
 default_lon = -1.259789
 push_url = 'https://api.pushbullet.com/v2/pushes'
-push_token = 'o.z1vYtJOsObfsxwzHmHdC3Ha1frqDUXNO'
+push_token = os.environ('PUSHBULLET_TOKEN')
+if debug:
+    print(push_token)
