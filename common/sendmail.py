91323e562120b7eed812d8b173cc3b9975976564
Olivier Le Page <olivier.le-page@capgemini.com>
2021-02-09 15:21:10 +0100

    Fix username issue

--- common/sendmail.py
+++ common/sendmail.py
@@ -6,8 +6,8 @@ import common.constants
 def send_mail(subject: str, message: str) -> bool:
     with smtplib.SMTP('smtp.live.com') as server:
         server.starttls()
-        server.login(common.constants.smtp_username, common.constants.SMTP_PASSWORD)
-        from_address = common.constants.smtp_username
+        server.login(common.constants.SMTP_USERNAME, common.constants.SMTP_PASSWORD)
+        from_address = common.constants.SMTP_USERNAME
         to_addresses = ['olivier.lp@gmail.com']
         msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (from_address, ", ".join(to_addresses), subject, message)
 
