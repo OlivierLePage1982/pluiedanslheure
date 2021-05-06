687152a17568450dbed048aa65b657cd0e95ec53
Olivier Le Page <olivier.le-page@capgemini.com>
2021-05-06 20:01:04 +0200

    Catch OSError when connecting to SMTP

--- common/sendmail.py
+++ common/sendmail.py
@@ -4,21 +4,25 @@ import common.constants
 
 
 def send_mail(subject: str, message: str) -> bool:
-    with smtplib.SMTP('smtp.live.com') as server:
-        server.starttls()
-        server.login(common.constants.SMTP_USERNAME, common.constants.SMTP_PASSWORD)
-        from_address = common.constants.SMTP_USERNAME
-        to_addresses = ['olivier.lp@gmail.com']
-        msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (from_address, ", ".join(to_addresses), subject, message)
-
-        try:
-            server.sendmail(from_address, to_addresses, msg)
-            logging.info('Mail notification sent to ' + '|'.join(to_addresses) + ' successfully')
-        except Exception as e:
-            logging.warning('Unable to send mail notification: ', e)
-            return False
-        server.quit()
-    return True
+    try:
+        with smtplib.SMTP('smtp.live.com') as server:
+            server.starttls()
+            server.login(common.constants.SMTP_USERNAME, common.constants.SMTP_PASSWORD)
+            from_address = common.constants.SMTP_USERNAME
+            to_addresses = ['olivier.lp@gmail.com']
+            msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (from_address, ", ".join(to_addresses), subject, message)
+    
+            try:
+                server.sendmail(from_address, to_addresses, msg)
+                logging.info('Mail notification sent to ' + '|'.join(to_addresses) + ' successfully')
+            except Exception as e:
+                logging.warning('Unable to send mail notification: ', e)
+                return False
+            server.quit()
+        return True
+    except Exception as e:
+        logging.warning('Unable to connect to SMTP server: ', e)
+        return False
 
 
 if __name__ == '__main__':
